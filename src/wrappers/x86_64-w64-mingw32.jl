86ccf7656e5db710c95ff13e7f5929d69a11e6de
jlbuild <juliabuildbot@gmail.com>
2024-02-25 19:40:49 +0000

    FFplay_jll build 6.1.1+0

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -3,10 +3,11 @@ export ffplay
 
 using FFMPEG_jll
 using SDL2_jll
+using OpenSSL_jll
 JLLWrappers.@generate_wrapper_header("FFplay")
 JLLWrappers.@declare_executable_product(ffplay)
 function __init__()
-    JLLWrappers.@generate_init_header(FFMPEG_jll, SDL2_jll)
+    JLLWrappers.@generate_init_header(FFMPEG_jll, SDL2_jll, OpenSSL_jll)
     JLLWrappers.@init_executable_product(
         ffplay,
         "bin\\ffplay.exe",
